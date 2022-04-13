b2738531250e14582d69267e504b60e04bbb29de
AlissonMarques <50666664+alissonthx@users.noreply.github.com>
2022-04-13 10:45:34 -0300

    add countdown script js

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,18 +1,29 @@
-const newYears = '1st jan 2021'
+const newYears = "1 jan 2023";
+
+const daysElement = document.getElementById("days");
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
 
 function countdown() {
-  const newYearsDate = new Date(newYears)
-  const currentDate = new Date()
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+  
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  const seconds = (newYearsDate - currentDate) * 1000
+  const seconds = Math.floor(totalSeconds) % 60;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = (Math.floor(totalSeconds / 60) % 24) % 60;
 
-  const days = seconds / 3600 / 24
-  const hours = Math.floor(seconds / 3600) % 24
-  const minutes = (Math.floor(seconds / 60) % 24) % 60
+  console.log(days, hours, minutes, seconds);
 
-  console.log(days, hours, minutes)
+  daysElement.innerHTML = days;
+  hoursElement.innerHTML = hours;
+  minutesElement.innerHTML = minutes;
+  secondsElement.innerHTML = seconds;
 }
 
-countdown()
+countdown();
 
-setInterval(countdown, 1000)
+setInterval(countdown, 1000);
